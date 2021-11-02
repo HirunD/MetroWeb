7305ebea466ff29342728d182b12d90e0fde056a
HirunD <hirundwickramasinghe@gmail.com>
2021-11-03 01:11:11 +0530

    cursor

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('mousemove'
 , function(e){
     var x = e.pageX;
     var y = e.pageY;
-    document.getElementById("cursor").style.left = 70 + x + "px";
+    document.getElementById("cursor").style.left = x + "px";
     document.getElementById("cursor").style.top =  y + "px";
 }
 )
