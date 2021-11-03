1dd9bb2429242922d6bdf3dc9cc51713a8b8e306
HirunD <hirundwickramasinghe@gmail.com>
2021-11-03 08:27:24 +0530

    database API connected

--- app.js
+++ app.js
@@ -6,3 +6,28 @@ document.addEventListener('mousemove'
     document.getElementById("cursor").style.top =  y + "px";
 }
 )
+
+
+
+// api url
+const api_url = 
+      "https://sheetdb.io/api/v1/meavkqmohxs7d";
+
+      async function getapi(url) {
+    
+        // Storing response
+        const response = await fetch(url);
+        
+        // Storing data in form of JSON
+        var data = await response.json();
+        console.log(data);
+        var categorise = data.results;  
+        return categorise.map(function(author) {
+            let ct = categorise.Category;
+        })
+
+        document.getElementById("bannert").title = ct;
+}
+    
+    // Calling that async function
+    getapi(api_url);
