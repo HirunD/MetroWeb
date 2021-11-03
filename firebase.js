530553484c59202cdcc20880a78fdfb82ebe07c1
HirunD <hirundwickramasinghe@gmail.com>
2021-11-03 20:32:30 +0530

    minor database errors

--- firebase.js
+++ firebase.js
@@ -1,20 +1,26 @@
 const title = document.querySelector('#bannert');
 const byname = document.querySelector('#byname');
 const para = document.querySelector('#articlepara')
+const list = document.querySelector('#toparticles')
 
 function renderTitle(doc){
   let title = document.getElementById('bannert');
   let byname = document.getElementById('byname');
   let para = document.getElementById('articlepara');
+  let li = document.createElement('li');
+  let name = document.createElement('span')
+
 
   title.setAttribute('data-id', doc.id);
   byname.setAttribute('data-id', doc.id);
   para.setAttribute('data-id', doc.id);
+  li.setAttribute('data-id', doc.id);
 
 
   title.textContent = doc.data().category.article.a_name;
   byname.textContent = doc.data().category.article.a_posted_by;
   para.textContent = doc.data().category.article.a_content;
+  name.textContent = doc.data().1st.a_name;
 
 }
 
