d7639ddd2e81bb53b6b0c19dcc4bc7c3db015895
HirunD <hirundwickramasinghe@gmail.com>
2021-11-05 05:18:13 +0530

    minor firebase issues

--- firebase.js
+++ firebase.js
@@ -10,7 +10,7 @@ function renderTitle(doc){
   let title = document.getElementById('bannert');
   let byname = document.getElementById('byname');
   let para = document.getElementById('articlepara');
-  // let li = document.createElement('li');
+  let li = document.createElement('li');
   let tag = document.createElement('li');
   let date = document.createElement('p');
 
@@ -33,7 +33,7 @@ function renderTitle(doc){
 }
 
 //getting data
-db.collection('MetroWeb').get().then((snapshot)=>{
+db.collection('MetroWeb', 'ocJ1W3dumwB5oOOk23aY').get().then((snapshot)=>{
   console.log(snapshot.docs);
   snapshot.docs.forEach(doc => {
     renderTitle(doc);
@@ -47,15 +47,20 @@ db.collection('MetroWeb').get().then((snapshot)=>{
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   db.collection('MetroWeb').add({
-    title: form.title.value,
-    byname: form.name.value,
-    para: form.context.value,
-    tag: form.tag.value,
-    date: form.date.value
+    category :{
+      article:{
+      a_name: form.title.value,
+      a_posted_by: form.name.value,
+      a_content: form.context.value,
+      a_tags: form.tag.value,
+      a_date: form.date.value
+      }
+    },
+    c_name : form.category.value
   });
   form.title.value = '';
   form.name.value = '';
   form.context.value = '';
   form.tag.value = '';
-  form.date.value = '';
+  form.date.value = ''
 });
