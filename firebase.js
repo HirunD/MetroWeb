ddf774ec6dacd0a98fd84cdd37b437a120a7da7c
HirunD <hirundwickramasinghe@gmail.com>
2021-11-04 10:30:55 +0530

    ...

--- firebase.js
+++ firebase.js
@@ -1,27 +1,29 @@
 const title = document.querySelector('#bannert');
 const byname = document.querySelector('#byname');
-const para = document.querySelector('#articlepara')
-const list = document.querySelector('#toparticles')
+const para = document.querySelector('#articlepara');
+const list = document.querySelector('#li');
 
 function renderTitle(doc){
   let title = document.getElementById('bannert');
   let byname = document.getElementById('byname');
   let para = document.getElementById('articlepara');
-  let li = document.createElement('li');
-  let name = document.createElement('span')
+  // let li = document.createElement('li');
+  let name = document.createElement('li');
 
 
   title.setAttribute('data-id', doc.id);
   byname.setAttribute('data-id', doc.id);
   para.setAttribute('data-id', doc.id);
-  li.setAttribute('data-id', doc.id);
+  name.setAttribute('data-id', doc.id);
 
 
   title.textContent = doc.data().category.article.a_name;
   byname.textContent = doc.data().category.article.a_posted_by;
   para.textContent = doc.data().category.article.a_content;
-  name.textContent = doc.data().1st.a_name;
+  name.textContent = doc.data().category.article.a_tags;
 
+
+  list.appendChild(li);
 }
 
 
