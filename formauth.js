d7c38d2c2673b94a7fd81b05b483851cbb4d3118
HirunD <hirundwickramasinghe@gmail.com>
2021-11-10 15:57:06 +0530

    article submission condition

--- formauth.js
+++ formauth.js
@@ -1,29 +1,29 @@
-// const form = document.querySelector('#submitform');
+const form = document.querySelector('#submitform');
 
-// const setupIU = (user) => {
-//   if (user) {
-//     form.style.display = 'block';
-//   } else{
-//     form.style.display = 'none';
-//   }
-// }
+const setupIU = (user) => {
+  if (user) {
+    form.style.display = 'block';
+  } else{
+    form.style.display = 'none';
+  }
+}
 
 
-// //listen for auth status changes
+//listen for auth status changes
 
-// auth.onAuthStateChanged((user) => {
-//     if (user) {
-//       console.log('logged_in');
-//       setupIU(user);
-//     } else {
-//       console.log('logged_out');
-//       setupIU();
-//     }
-//   });
+auth.onAuthStateChanged((user) => {
+    if (user) {
+      console.log('logged_in');
+      setupIU(user);
+    } else {
+      console.log('logged_out');
+      setupIU();
+    }
+  });
   
   
-// const logout = document.querySelector('#logout');
+const logout = document.querySelector('#logout');
 
-// logout.addEventListener('click', (e) =>{
-//   auth.signOut();}
-// );
+logout.addEventListener('click', (e) =>{
+  auth.signOut();}
+);
